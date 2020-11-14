71d50d72cf2401f05831b5547979d78cb68ab5ed
Jdup29 <judup13@gmail.com>
2020-11-13 20:44:48 -0500

    battery and column (type and model)

---
+++ app/graphql/types/column_type.rb
@@ -0,0 +1,14 @@
+module Types
+    class ColumnType < Types::BaseObject
+        field :id, ID, null: false
+        field :type_of_building, String, null: true
+        field :number_of_floors_served, Integer, null: true
+        field :status, String, null: true
+        field :information, String, null: true
+        field :notes, String, null: true
+        field :created_at, String, null: true
+        field :updated_at, String, null: true
+        field :battery_id, ID, null: true 
+        field :customer_id, ID, null: true 
+    end
+end
