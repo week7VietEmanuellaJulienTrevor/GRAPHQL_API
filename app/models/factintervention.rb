b412bc3b746486cdd228f1836db69ce093cf1dd4
Jdup29 <judup13@gmail.com>
2020-11-13 23:17:00 -0500

    final push

--- app/models/factintervention.rb
+++ app/models/factintervention.rb
@@ -1,7 +1,4 @@
 class Factintervention < DatawarehouseBase
     belongs_to :building, validate: true
-    # belongs_to :employees
-    # belongs_to :batteries
-    # belongs_to :columns
-    # belongs_to :elevators
+
 end
